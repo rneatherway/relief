b9c96ff5ea3fc22d6829ed5f886985f874bb57fb
Robin Neatherway <rneatherway@github.com>
2021-10-25 00:09:21 +0100

    a lot

--- cmd/tiffgdal/tiff3.go
+++ cmd/tiffgdal/tiff3.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
 	"os"
+	"os/exec"
+	"path"
+	"strings"
 
 	"github.com/airbusgeo/godal"
 	"github.com/hschendel/stl"
@@ -10,8 +18,41 @@ import (
 
 type PixelBuffer struct {
 	buf    []float32
-	width  int
-	height int
+	width  uint
+	height uint
+
+	minMaxComputed bool
+	min            float32
+	max            float32
+}
+
+func (pb *PixelBuffer) minMax() {
+	pb.min = pb.buf[0]
+	pb.max = pb.buf[0]
+	for _, p := range pb.buf {
+		if p < pb.min {
+			pb.min = p
+		}
+
+		if p > pb.max {
+			pb.max = p
+		}
+	}
+	pb.minMaxComputed = true
+}
+
+func (pb *PixelBuffer) Min() float32 {
+	if !pb.minMaxComputed {
+		pb.minMax()
+	}
+	return pb.min
+}
+
+func (pb *PixelBuffer) Max() float32 {
+	if !pb.minMaxComputed {
+		pb.minMax()
+	}
+	return pb.max
 }
 
 // Imagine width is three, height is two and pixel data is:
@@ -22,90 +63,137 @@ type PixelBuffer struct {
 // This will be in the buf as: a b c d e f
 // So for each 'y' we need to advance by 'width'
 // x + y*width
-func (pb *PixelBuffer) get(x int, y int) float32 {
+func (pb *PixelBuffer) get(x uint, y uint) float32 {
 	return pb.buf[x+y*pb.width]
 }
 
-func (pb *PixelBuffer) getVec3(x int, y int) stl.Vec3 {
+func (pb *PixelBuffer) getVec3(x uint, y uint) stl.Vec3 {
 	return stl.Vec3{
 		float32(x),
 		float32(y),
-		pb.get(x, y),
+		pb.get(x, pb.height-y-1), // STL coordinate system is inverted, so flip y
 	}
 }
 
-func (pb *PixelBuffer) toSTL(x1 int, y1 int, x2 int, y2 int) stl.Solid {
-	solid := stl.Solid{}
+func AppendRectangle(solid *stl.Solid, tl, tr, bl, br stl.Vec3) {
+	solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
+	solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+}
+
+func (pb *PixelBuffer) toSTL() *stl.Solid {
+	solid := &stl.Solid{}
+
+	// The code below supports only converting a window, but
+	// I moved that to the GeoTIFF parsing.
+	x1 := uint(0)
+	y1 := uint(0)
+	x2 := pb.width - 1
+	y2 := pb.height - 1
 
 	// Top
 	for i := x1; i < x2; i++ {
 		for j := y1; j < y2; j++ {
-			// TODO: I think we can factor out this 4 points -> 2 triangles thing
-			tl := pb.getVec3(i, j)
-			tr := pb.getVec3(i+1, j)
-			bl := pb.getVec3(i, j+1)
-			br := pb.getVec3(i+1, j+1)
-
-			solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
-			solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+			AppendRectangle(
+				solid,
+				pb.getVec3(i, j),
+				pb.getVec3(i+1, j),
+				pb.getVec3(i, j+1),
+				pb.getVec3(i+1, j+1))
 		}
 	}
 
 	for i := x1; i < x2; i++ {
 		// Back
-		tr := pb.getVec3(i, 0)
-		tl := pb.getVec3(i+1, 0)
-		br := stl.Vec3{float32(i), 0, 0}
-		bl := stl.Vec3{float32(i + 1), 0, 0}
-
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+		AppendRectangle(
+			solid,
+			pb.getVec3(i+1, y1),
+			pb.getVec3(i, y1),
+			stl.Vec3{float32(i + 1), float32(y1), 0},
+			stl.Vec3{float32(i), float32(y1), 0},
+		)
 
 		// Front
-		tl = pb.getVec3(i, y2)
-		tr = pb.getVec3(i+1, y2)
-		bl = stl.Vec3{float32(i), float32(y2), 0}
-		br = stl.Vec3{float32(i + 1), float32(y2), 0}
-
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+		AppendRectangle(
+			solid,
+			pb.getVec3(i, y2),
+			pb.getVec3(i+1, y2),
+			stl.Vec3{float32(i), float32(y2), 0},
+			stl.Vec3{float32(i + 1), float32(y2), 0},
+		)
 	}
 
 	for j := y1; j < y2; j++ {
 		// Left
-		tr := pb.getVec3(0, j+1)
-		tl := pb.getVec3(0, j)
-		br := stl.Vec3{0, float32(j + 1), 0}
-		bl := stl.Vec3{0, float32(j), 0}
-
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+		AppendRectangle(
+			solid,
+			pb.getVec3(x1, j),
+			pb.getVec3(x1, j+1),
+			stl.Vec3{float32(x1), float32(j), 0},
+			stl.Vec3{float32(x1), float32(j + 1), 0},
+		)
 
 		// Right
-		tl = pb.getVec3(x2, j+1)
-		tr = pb.getVec3(x2, j)
-		bl = stl.Vec3{float32(x2), float32(j + 1), 0}
-		br = stl.Vec3{float32(x2), float32(j), 0}
-
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
-		solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+		AppendRectangle(
+			solid,
+			pb.getVec3(x2, j+1),
+			pb.getVec3(x2, j),
+			stl.Vec3{float32(x2), float32(j + 1), 0},
+			stl.Vec3{float32(x2), float32(j), 0},
+		)
 	}
 
 	// Bottom
-	tl := stl.Vec3{0, float32(y2), 0}
-	tr := stl.Vec3{float32(x2), float32(y2), 0}
-	bl := stl.Vec3{0, 0, 0}
-	br := stl.Vec3{float32(x2), 0, 0}
-
-	solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tl, bl, tr}})
-	solid.AppendTriangle(stl.Triangle{Vertices: [3]stl.Vec3{tr, bl, br}})
+	AppendRectangle(
+		solid,
+		stl.Vec3{float32(x1), float32(y2), 0},
+		stl.Vec3{float32(x2), float32(y2), 0},
+		stl.Vec3{float32(x1), float32(y1), 0},
+		stl.Vec3{float32(x2), float32(y1), 0},
+	)
 
 	solid.RecalculateNormals()
 	solid.Validate()
 	return solid
 }
 
-func FromGeoTIFF(path string) (*PixelBuffer, error) {
+func (pb *PixelBuffer) ToImage() image.Image {
+	r := image.Rect(0, 0, int(pb.width)-1, int(pb.height)-1)
+	img := image.NewNRGBA(r)
+
+	fmt.Printf("Rescaling image with height min %f and max %f\n", pb.Min(), pb.Max())
+	for i := uint(0); i < pb.width; i++ {
+		for j := uint(0); j < pb.height; j++ {
+			z := pb.get(i, j)
+			c := 65535 * (z - pb.Min()) / (pb.Max() - pb.Min())
+			img.Set(int(i), int(j), color.Gray16{uint16(c)})
+		}
+	}
+	return img
+}
+
+func (pb *PixelBuffer) Zero() {
+	if len(pb.buf) == 0 {
+		return
+	}
+
+	for i := range pb.buf {
+		pb.buf[i] = pb.buf[i] - pb.Min()
+	}
+}
+
+func (pb *PixelBuffer) Scale(s float32) {
+	for i := range pb.buf {
+		pb.buf[i] = pb.buf[i] * s
+	}
+}
+
+func (pb *PixelBuffer) Diff(pb2 *PixelBuffer) {
+	for i := range pb.buf {
+		pb.buf[i] = pb.buf[i] - pb2.buf[i]
+	}
+}
+
+func FromGeoTIFF(path string, x, y, w, h uint) (*PixelBuffer, error) {
 	godal.RegisterAll()
 	hDataset, err := godal.Open(path)
 	if err != nil {
@@ -114,379 +202,132 @@ func FromGeoTIFF(path string) (*PixelBuffer, error) {
 	defer hDataset.Close()
 
 	structure := hDataset.Structure()
+
+	if uint(structure.SizeX) < x+w {
+		return nil, fmt.Errorf("selected window goes outside image bounds (image width=%d, window max x=%d)", structure.SizeX, x+w)
+	}
+	if uint(structure.SizeY) < y+h {
+		return nil, fmt.Errorf("selected window goes outside image bounds (image height=%d, window max y=%d)", structure.SizeY, y+h)
+	}
+
+	if w == 0 {
+		w = uint(structure.SizeX) - x
+	}
+	if h == 0 {
+		h = uint(structure.SizeY) - y
+	}
+
 	band := hDataset.Bands()[0]
-	buf := make([]float32, structure.SizeX*structure.SizeY)
-	err = band.Read(0, 0, buf, structure.SizeX, structure.SizeY)
+	buf := make([]float32, w*h)
+	fmt.Printf("Reading GeoTIFF band %d (of %d) window (%dx%d+%dx%d) into buffer size %d...",
+		1, len(hDataset.Bands()), x, y, w, h, len(buf))
+	err = band.Read(int(x), int(y), buf, int(w), int(h))
+	fmt.Println("done")
 	if err != nil {
 		return nil, err
 	}
 
-	return &PixelBuffer{buf: buf, width: structure.SizeX, height: structure.SizeY}, nil
+	// Set undefined to zero, for now
+	for i := range buf {
+		if buf[i] == -math.MaxFloat32 {
+			buf[i] = 0
+		}
+	}
+
+	return &PixelBuffer{buf: buf, width: w, height: h}, nil
 }
 
 func realMain() error {
-	pb, err := FromGeoTIFF("P_10719/DTM_SP5005_P_10719_20200315_20200315.tif")
+	x := flag.Uint("x", 0, "x coordinate (default 0)")
+	y := flag.Uint("y", 0, "y coordinate (default 0)")
+	w := flag.Uint("w", 0, "width (default max)")
+	h := flag.Uint("h", 0, "height (default max)")
+	s := flag.Float64("s", 1, "scale vertically (default 1)")
+	zero := flag.Bool("z", false, "use the lowest height as 'zero'")
+	diff := flag.String("d", "", "a second file to compare against")
+	visualize := flag.Bool("v", false, "visualize the model")
+	output := flag.String("output", "out.stl", "output STL file (default out.stl)")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s [OPTIONS] <input geotiff file>:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	if path.Ext(*output) != ".stl" && path.Ext(*output) != ".png" {
+		return fmt.Errorf("unsupported output format")
+	}
+
+	fmt.Println(flag.NArg())
+	switch flag.NArg() {
+	case 0:
+		flag.Usage()
+		return fmt.Errorf("no input file given")
+	case 1:
+		// Great
+	default:
+		flag.Usage()
+		return fmt.Errorf("unrecognised arguments %s", strings.Join(flag.Args()[1:], ", "))
+	}
+	input := flag.Arg(0)
+	pb, err := FromGeoTIFF(input, *x, *y, *w, *h)
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Println(pb.get(0, 0))
-	return err
+	if *diff != "" {
+		pb2, err := FromGeoTIFF(*diff, *x, *y, *w, *h)
+		if err != nil {
+			return err
+		}
+		pb.Diff(pb2)
+	}
+
+	if *zero {
+		fmt.Print("Setting minimum height value to 0...")
+		pb.Zero()
+		fmt.Println("done")
+	}
+
+	if *s != 1.0 {
+		fmt.Printf("Adjusting vertical scale by factor of %f...", *s)
+		pb.Scale(float32(*s))
+		fmt.Println("done")
+	}
+
+	switch path.Ext(*output) {
+	case ".stl":
+		fmt.Printf("Converting to STL file '%s'...", *output)
+		err = pb.toSTL().WriteFile(*output)
+		if err != nil {
+			return err
+		}
+		fmt.Println("done")
+
+		if *visualize {
+			fmt.Println("Launching visualisation")
+			return exec.Command("f3d", *output).Run()
+		}
+	case ".png":
+		fmt.Printf("Converting to PNG file '%s'...", *output)
+		img := pb.ToImage()
+		f, err := os.Create(*output)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		err = png.Encode(f, img)
+		if err != nil {
+			return err
+		}
+		fmt.Println("done")
+	}
+
+	return nil
 }
 
 func main() {
 	err := realMain()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
-	return
-	/*
-		GDALDatasetH  hDataset;
-		GDALAllRegister();
-		hDataset = GDALOpen( pszFilename, GA_ReadOnly );
-		if( hDataset == NULL )
-		{
-			...;
-		}
-	*/
-	godal.RegisterAll()
-	hDataset, err := godal.Open("P_10719/DTM_SP5005_P_10719_20200315_20200315.tif")
-	if err != nil {
-		panic(err)
-	}
-
-	/*
-		hDriver = GDALGetDatasetDriver( hDataset );
-		printf( "Driver: %s/%s\n",
-			GDALGetDriverShortName( hDriver ),
-			GDALGetDriverLongName( hDriver ) );
-	*/
-	//not implemented
-
-	/*
-		printf( "Size is %dx%dx%d\n",
-			GDALGetRasterXSize( hDataset ),
-			GDALGetRasterYSize( hDataset ),
-			GDALGetRasterCount( hDataset ) );
-	*/
-	structure := hDataset.Structure()
-	fmt.Printf("Size is %dx%dx%d\n", structure.SizeX, structure.SizeY, structure.NBands)
-
-	/*
-		if( GDALGetProjectionRef( hDataset ) != NULL )
-			printf( "Projection is '%s'\n", GDALGetProjectionRef( hDataset ) );
-	*/
-	if pj := hDataset.Projection(); pj != "" {
-		fmt.Printf("Projection is '%s'\n", pj)
-	}
-
-	/*
-		if( GDALGetGeoTransform( hDataset, adfGeoTransform ) == CE_None )
-		{
-			printf( "Origin = (%.6f,%.6f)\n",
-				adfGeoTransform[0], adfGeoTransform[3] );
-			printf( "Pixel Size = (%.6f,%.6f)\n",
-				adfGeoTransform[1], adfGeoTransform[5] );
-		}
-	*/
-	if gt, err := hDataset.GeoTransform(); err == nil {
-		fmt.Printf("Origin = (%.6f,%.6f)\n", gt[0], gt[3])
-		fmt.Printf("Pixel Size = (%.6f,%.6f)\n", gt[1], gt[5])
-	}
-
-	/*
-		GDALRasterBandH hBand;
-		int             nBlockXSize, nBlockYSize;
-		int             bGotMin, bGotMax;
-		double          adfMinMax[2];
-		hBand = GDALGetRasterBand( hDataset, 1 );
-		GDALGetBlockSize( hBand, &nBlockXSize, &nBlockYSize );
-		printf( "Block=%dx%d Type=%s, ColorInterp=%s\n",
-				nBlockXSize, nBlockYSize,
-				GDALGetDataTypeName(GDALGetRasterDataType(hBand)),
-				GDALGetColorInterpretationName(
-					GDALGetRasterColorInterpretation(hBand)) );
-	*/
-	band := hDataset.Bands()[0] //Note that in godal, bands are indexed starting from 0, not 1
-	bandStructure := band.Structure()
-	fmt.Printf("Block=%dx%d Type=%s, ColorInterp=%s\n",
-		bandStructure.BlockSizeX, bandStructure.BlockSizeY,
-		bandStructure.DataType,
-		band.ColorInterp().Name())
-
-	buf := make([]float32, structure.SizeX*structure.SizeY)
-	err = band.Read(0, 0, buf, structure.SizeX, structure.SizeY)
-	if err != nil {
-		panic(err)
-	}
-
-	solid := stl.Solid{}
-	maxX := 10
-	maxY := 10
-	for i := 0; i < maxX-1; i++ {
-		for j := 0; j < maxY-1; j++ {
-			solid.AppendTriangle(
-				stl.Triangle{
-					Vertices: [3]stl.Vec3{
-						{float32(i), float32(j), buf[i+j*structure.SizeX]},
-						{float32(i + 1), float32(j), buf[i+1+j*structure.SizeX]},
-						{float32(i), float32(j + 1), buf[i+(j+1)*structure.SizeX]},
-					},
-				},
-			)
-			solid.AppendTriangle(
-				stl.Triangle{
-					Vertices: [3]stl.Vec3{
-						{float32(i + 1), float32(j), buf[i+1+j*structure.SizeX]},
-						{float32(i + 1), float32(j + 1), buf[i+1+(j+1)*structure.SizeX]},
-						{float32(i), float32(j + 1), buf[i+(j+1)*structure.SizeX]},
-					},
-				},
-			)
-		}
-	}
-
-	for i := 0; i < maxX-1; i++ {
-		// Front
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{float32(i), 0, 0},
-					{float32(i + 1), 0, 0},
-					{float32(i), 0, buf[i]},
-				},
-			},
-		)
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{float32(i + 1), 0, 0},
-					{float32(i + 1), 0, buf[i+1]},
-					{float32(i), 0, buf[i]},
-				},
-			},
-		)
-
-		// Back
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{float32(i), float32(maxY), 0},
-					{float32(i + 1), float32(maxY), 0},
-					{float32(i), float32(maxY), buf[i+maxY*structure.SizeX]},
-				},
-			},
-		)
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{float32(i + 1), float32(maxY), 0},
-					{float32(i + 1), float32(maxY), buf[i+1+maxY*structure.SizeX]},
-					{float32(i), float32(maxY), buf[i+maxY*structure.SizeX]},
-				},
-			},
-		)
-	}
-
-	for j := 0; j < maxY-1; j++ {
-		// Left
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{0, float32(j), 0},
-					{0, float32(j + 1), 0},
-					{0, float32(j), buf[j*structure.SizeY]},
-				},
-			},
-		)
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{0, float32(j + 1), 0},
-					{0, float32(j + 1), buf[(j+1)*structure.SizeY]},
-					{0, float32(j), buf[j*structure.SizeY]},
-				},
-			},
-		)
-
-		// Right
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{float32(maxX), float32(j), 0},
-					{float32(maxX), float32(j + 1), 0},
-					{float32(maxX), float32(j), buf[maxX+j*structure.SizeY]},
-				},
-			},
-		)
-		solid.AppendTriangle(
-			stl.Triangle{
-				Vertices: [3]stl.Vec3{
-					{float32(maxX), float32(j + 1), 0},
-					{float32(maxX), float32(j + 1), buf[maxX+(j+1)*structure.SizeY]},
-					{float32(maxX), float32(j), buf[maxX+j*structure.SizeY]},
-				},
-			},
-		)
-	}
-
-	// Base
-	solid.AppendTriangle(
-		stl.Triangle{
-			Vertices: [3]stl.Vec3{
-				{0.0, 0.0, 0.0},
-				{float32(maxX), 0.0, 0.0},
-				{0.0, float32(maxY), 0.0},
-			},
-		},
-	)
-	solid.AppendTriangle(
-		stl.Triangle{
-			Vertices: [3]stl.Vec3{
-				{float32(maxX), 0.0, 0.0},
-				{float32(maxX), float32(maxY), 0.0},
-				{0.0, float32(maxY), 0.0},
-			},
-		},
-	)
-
-	solid.RecalculateNormals()
-	solid.Validate()
-	solid.WriteFile("relief.stl")
-	return
-	/*
-		adfMinMax[0] = GDALGetRasterMinimum( hBand, &bGotMin );
-		adfMinMax[1] = GDALGetRasterMaximum( hBand, &bGotMax );
-		if( ! (bGotMin && bGotMax) )
-			GDALComputeRasterMinMax( hBand, TRUE, adfMinMax );
-		printf( "Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1] );
-	*/
-	//not implemented
-
-	/*
-		if( GDALGetOverviewCount(hBand) > 0 )
-			printf( "Band has %d overviews.\n", GDALGetOverviewCount(hBand));
-	*/
-	if overviews := band.Overviews(); len(overviews) > 0 {
-		fmt.Printf("Band has %d overviews.\n", len(overviews))
-	}
-
-	/*
-		if( GDALGetRasterColorTable( hBand ) != NULL )
-			printf( "Band has a color table with %d entries.\n",
-					GDALGetColorEntryCount(
-						GDALGetRasterColorTable( hBand ) ) );
-	*/
-	if ct := band.ColorTable(); len(ct.Entries) > 0 {
-		fmt.Printf("Band has a color table with %d entries.\n", len(ct.Entries))
-	}
-
-	/*
-		float *pafScanline;
-		int   nXSize = GDALGetRasterBandXSize( hBand );
-		pafScanline = (float *) CPLMalloc(sizeof(float)*nXSize);
-		GDALRasterIO( hBand, GF_Read, 0, 0, nXSize, 1,
-			pafScanline, nXSize, 1, GDT_Float32,
-			0, 0 );
-	*/
-
-	pafScanline := make([]float32, structure.SizeX)
-	err = band.Read(0, 0, pafScanline, bandStructure.SizeX, 1)
-	if err != nil {
-		panic(err)
-	}
-
-	err = hDataset.Close()
-	// we don't really need to check for errors here as we have a read-only dataset.
-	if err != nil {
-		panic(err)
-	}
-
-	/*
-		const char *pszFormat = "GTiff";
-		GDALDriverH hDriver = GDALGetDriverByName( pszFormat );
-		char **papszMetadata;
-		if( hDriver == NULL )
-		    exit( 1 );
-		papszMetadata = GDALGetMetadata( hDriver, NULL );
-		if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATE, FALSE ) )
-		    printf( "Driver %s supports Create() method.\n", pszFormat );
-		if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATECOPY, FALSE ) )
-		    printf( "Driver %s supports CreateCopy() method.\n", pszFormat );
-	*/
-
-	hDriver, ok := godal.RasterDriver("Gtiff")
-	if !ok {
-		panic("Gtiff not found")
-	}
-	md := hDriver.Metadatas()
-	if md["DCAP_CREATE"] == "YES" {
-		fmt.Printf("Driver GTiff supports Create() method.\n")
-	}
-	if md["DCAP_CREATECOPY"] == "YES" {
-		fmt.Printf("Driver Gtiff supports CreateCopy() method.\n")
-	}
-
-	/*	GDALDataset *poSrcDS = (GDALDataset *) GDALOpen( pszSrcFilename, GA_ReadOnly );
-		GDALDataset *poDstDS;
-		char **papszOptions = NULL;
-		papszOptions = CSLSetNameValue( papszOptions, "TILED", "YES" );
-		papszOptions = CSLSetNameValue( papszOptions, "COMPRESS", "PACKBITS" );
-		poDstDS = poDriver->CreateCopy( pszDstFilename, poSrcDS, FALSE,
-										papszOptions, GDALTermProgress, NULL );
-		if( poDstDS != NULL )
-			GDALClose( (GDALDatasetH) poDstDS );
-		CSLDestroy( papszOptions );
-
-		GDALClose( (GDALDatasetH) poSrcDS );
-	*/
-
-	//Left out: dealing with error handling
-	poSrcDS, _ := godal.Open("testdata/test.tif")
-	pszDstFilename := "/vsimem/tempfile.tif"
-	defer godal.VSIUnlink(pszDstFilename)
-	//godal doesn't expose createCopy directly, but the same result can be obtained with Translate
-	poDstDS, _ := poSrcDS.Translate(pszDstFilename, nil, godal.CreationOption("TILED=YES", "COMPRESS=PACKBITS"), godal.GTiff)
-	poDstDS.Close()
-	poSrcDS.Close()
-
-	/*
-		GDALDataset *poDstDS;
-		char **papszOptions = NULL;
-		poDstDS = poDriver->Create( pszDstFilename, 512, 512, 1, GDT_Byte,
-									papszOptions );
-		double adfGeoTransform[6] = { 444720, 30, 0, 3751320, 0, -30 };
-		OGRSpatialReference oSRS;
-		char *pszSRS_WKT = NULL;
-		GDALRasterBand *poBand;
-		GByte abyRaster[512*512];
-		poDstDS->SetGeoTransform( adfGeoTransform );
-		oSRS.SetUTM( 11, TRUE );
-		oSRS.SetWellKnownGeogCS( "NAD27" );
-		oSRS.exportToWkt( &pszSRS_WKT );
-		poDstDS->SetProjection( pszSRS_WKT );
-		CPLFree( pszSRS_WKT );
-		poBand = poDstDS->GetRasterBand(1);
-		poBand->RasterIO( GF_Write, 0, 0, 512, 512,
-						abyRaster, 512, 512, GDT_Byte, 0, 0 );
-		GDALClose( (GDALDatasetH) poDstDS );
-	*/
-
-	poDstDS, _ = godal.Create(godal.GTiff, pszDstFilename, 1, godal.Byte, 512, 512)
-	defer poDstDS.Close() //Close can be defered / called more than once (second+ calls are no-ops)
-
-	poDstDS.SetGeoTransform([6]float64{444720, 30, 0, 3751320, 0, -30})
-
-	//SetUTM and SetWellKnownGeogCS not implemented. godal allows populating
-	// a SpatialRef from a WKT or PROJ4 string, or an epsg code
-	sr, _ := godal.NewSpatialRefFromEPSG(4326)
-	defer sr.Close()
-	poDstDS.SetSpatialRef(sr)
-
-	abyRaster := make([]byte, 512*512)
-	// ... now populate with data
-	poDstDS.Bands()[0].Write(0, 0, abyRaster, 512, 512)
-	poDstDS.Close()
-
 }
--- cmd/tiffgdal/tiff3_test.go
+++ cmd/tiffgdal/tiff3_test.go
@@ -6,12 +6,12 @@ import (
 
 func TestAThing(t *testing.T) {
 	pb := &PixelBuffer{
-		buf:    []float32{1, 1, 1, 1, 1, 1.5, 1.2, 1, 1, 1, 1, 1},
+		buf:    []float32{1, 1.1, 1.2, 0.9, 1.4, 2.5, 1.9, 1.1, 0.3, 0.4, 0.8, 0.6},
 		width:  4,
 		height: 3,
 	}
 
-	solid := pb.toSTL(0, 0, 3, 2)
+	solid := pb.toSTL()
 
 	t.Logf("%d triangles", len(solid.Triangles))
 
